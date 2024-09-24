c1add9e9209035bed0a001a40137732b8322df8f
Admin <reteamboss@gmail.com>
2024-09-24 16:09:05 +0300

    3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,14 @@
 fun main() {
-    println("Hello World!")
+    val phoneToYear = listOf(
+        "Nexus One" to 2010,
+        "Pixel 2" to 2017,
+        "Pixel 4a" to 2020,
+        "iPhone 4" to 2010,
+        "iPhone X" to 2017,
+        "Galaxy Note 8" to 2017,
+        "Galaxy S11" to 2020
+    )
+
+    println(phoneToYear.groupBy { it.second })
 }
+
